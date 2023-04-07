32204c4a915338c3b30cc16a54826b222e37ebe0
minjikim <minjiii.kim12@gmail.com>
2023-04-07 13:38:49 +0900

    project setting

--- app/src/main/java/minji/kim/maeul/Listpage.java
+++ app/src/main/java/minji/kim/maeul/Listpage.java
@@ -10,5 +10,6 @@ public class Listpage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listpage);
+
     }
 }
--- app/src/main/java/minji/kim/maeul/Loginpage.java
+++ app/src/main/java/minji/kim/maeul/Loginpage.java
@@ -1,6 +1,9 @@
 package minji.kim.maeul;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,18 @@ public class Loginpage extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginpage);
+        setContentView(R.layout.loginpage);}
+
+
     }
-}
+
+    }
+
+
+
+
+
+
+
+
+
