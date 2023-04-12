67602aac50a32bffe4edcfbc1050dc19b4e2c562
minjikim <minjiii.kim12@gmail.com>
2023-04-12 17:00:05 +0900

    project setting

--- app/src/main/java/minji/kim/maeul/Donationpage.java
+++ app/src/main/java/minji/kim/maeul/Donationpage.java
@@ -1,6 +1,10 @@
 package minji.kim.maeul;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +14,14 @@ public class Donationpage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donationpage);
+
+        Button donate = findViewById(R.id.donate);
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
